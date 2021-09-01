b76b517561701f8dfcca1df11951314d94c57237
Andrew Kurakov <sniperaft@gmail.com>
2021-09-01 21:51:42 +0700

    Пояснение к приложению

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:start_get_eticon/screens/no_named_screen/no_named_screen.dart';
 
+//В данном приложении содержится два экрана. Вам необходимо добавить в это приложение Get.
+// И согласно комментариям в NoNamedScreen выполнить задания
+
 void main() {
   runApp(MyApp());
 }
